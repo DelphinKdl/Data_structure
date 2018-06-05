fe50df065e88fd36ab2656f4257d5f8ee6e7ad41
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-05 11:22:25 -0700

    inserting in the beginning of a lnked list

--- linkedLists/C++/linkedlist.cpp
+++ linkedLists/C++/linkedlist.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
-using namespace std ; 
+using namespace std; 
 struct Node {
     int data; 
     Node * next ; 
-};
-Node * add(Node *head, int data) {
+}; 
 
-    Node * temp  = new Node();
 
-    temp->data = data;
-    temp->next = nullptr;
-    head = temp;
+Node *add (Node * head, int item){
+    Node * temp = new Node();
+    temp->data = item ; 
+    temp->next = NULL; // Whu NULL vs nullpte? 
+    if (head == NULL) {
+        head = temp ;
+        return head;
+    } else {
+        // TODO: Insert items in the begninning of the lnkedlis
+        temp->next = head ; 
+        head = temp ;
+    }
     return head;
 }
+void print(Node * head) {
+    Node * temp = head; 
+    while(temp != NULL) {
+        cout<<temp->data<<endl;
+        temp = temp->next ; 
+    }
+
+}
 int main () {
-
-Node * head = NULL;
-
-head = add(head,12);
-
-cout<<head->data<<endl;
-
+    Node * head = NULL; 
+    head = add(head,10);
+    head = add(head,12);
+    head = add(head,14);
+    head = add(head,13);
+    print(head);
 
 }
