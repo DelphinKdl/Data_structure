0d6d79c600689a92bcf990613eb8cb6203619224
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-11 10:55:30 -0700

    added some code for doubly ls. It is easy

---
+++ DoublyLinkedList/DoublylinkedList.cpp
@@ -0,0 +1,58 @@
+// Doubly linked list is the same as the singly, except, we add a nother var to our node. 
+
+#include <iostream>
+using namespace std ;
+struct Node {
+    int data; 
+    Node *next ; 
+    Node * prev ; 
+
+};
+void printBackwards (Node * head){
+    Node * temp = head ; 
+    while (temp->next != NULL) {
+        temp = temp->next; 
+    }
+    Node *n = temp ; 
+    while (n != NULL) {
+        cout<<n->data<<" ";
+        n = n->prev; 
+    }
+    cout<<endl;
+}
+void printForward(Node * head) {
+    Node * temp = head ; 
+    while(temp != NULL) {
+        cout<<temp->data<<" ";
+        temp = temp->next; 
+    }
+    cout<<endl;
+}
+Node * insert(Node * head, int data){
+    Node * newNode = new Node();
+    newNode->data = data; 
+    newNode->next = NULL ; 
+    newNode->prev = NULL ; 
+
+    if (head == NULL) {
+        head = newNode;
+        return head; // this is not  a good code. 
+    } 
+        Node * temp = head; 
+        while (temp->next != NULL) {
+            temp = temp->next ; 
+        }
+        temp->next = newNode ; 
+        newNode->prev = temp ; 
+
+    return head;
+}
+int main() {
+
+Node * head = NULL ; 
+head = insert(head, 10);
+head = insert(head, 12);
+head = insert(head, 13);
+printForward(head);
+printBackwards(head);
+ }
