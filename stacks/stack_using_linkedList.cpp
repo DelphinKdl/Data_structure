40247b9b6db3f529174cb4885d4363bb5f21661c
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-13 01:03:00 -0700

    added some linked list for stack

--- stacks/stack_using_linkedList.cpp
+++ stacks/stack_using_linkedList.cpp
@@ -9,6 +9,50 @@ struct Node {
     Node * next ;
 
 };
-int main () {
 
+Node * push (Node* head, int data){
+    Node * newNode = new Node();
+    newNode->data = data ; 
+    newNode->next = NULL ;
+
+
+    if (head == NULL) {
+        head = newNode ;
+        return newNode ; 
+    } 
+    newNode->next = head ; 
+    head = newNode ; 
+    return head ; 
+}
+void print(Node * head) {
+    Node * temp = head; 
+    while(temp != NULL) {
+        cout<<temp->data<<endl;
+        temp = temp->next ; 
+    }
+
+}
+Node * pop (Node * head) {
+    if (head == NULL) {
+        cout<<"Stack is empty"<<endl;
+        return NULL;
+    }
+    Node * temp = head ; 
+    Node * newTemp = temp->next ; 
+    head = newTemp;
+    delete(temp);
+    return head; 
+
+}
+int main () {
+Node * head = NULL ; 
+head = push(head, 12);
+head = push(head, 1);
+head = push(head, 14);
+
+head = push(head, 15465);
+head= pop(head);
+
+
+print(head);
 }
