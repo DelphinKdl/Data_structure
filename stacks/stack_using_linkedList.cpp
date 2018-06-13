8024c1c6eb47914c0d6a11c3516e604e8ecbaeaa
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-13 00:47:22 -0700

    implement a stack using linked list

---
+++ stacks/stack_using_linkedList.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+
+// This is the implementation of stack using linkned list. 
+using namespace std ; 
+
+struct Node {
+    int data; 
+    Node * next ;
+
+};
+int main () {
+
+}
