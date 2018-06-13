0dec7e122b5be1866261037f4bc8b76af522eee8
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-13 03:28:44 -0700

    segemntation falut problem in check parenthesis balancing problem

---
+++ stacks/common_problems_using_stacks/balanced_parentheses.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std ; 
+stack<char> balanced_parenth; 
+
+char openings [3] = {'(','{','['};
+void check_balaanced_parenth(string exp) {
+    for (int i = 0 ; i<exp.length(); i++) {
+        if (exp[i] == '{' ||exp[i] == '[' || exp[i] == '(' ) {
+            balanced_parenth.push(exp[i]);
+        } else {
+            balanced_parenth.pop();
+        }
+    }
+}
+int main () {
+string expression = "][";
+// The algorithm: if opening
+
+check_balaanced_parenth(expression);
+if (balanced_parenth.empty()) {
+    cout<<"Expression is balanced"<<endl;
+} else {
+    cout<<"Expression is not balanaced"<<endl;
+}
+}
