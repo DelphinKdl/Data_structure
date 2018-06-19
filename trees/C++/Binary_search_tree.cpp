f59cc7e337ac2e74f6f0fc94e5cbdf6a0a6c8f51
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-19 15:55:48 -0700

    BST

---
+++ trees/C++/Binary_search_tree.cpp
@@ -0,0 +1,14 @@
+// You need to use  the class syntax for binary search tree.
+
+
+#include <iostream>
+
+using namespace std ;
+struct Node {
+    int data;
+    Node * left ;
+    Node * right;
+};
+int main () {
+    
+}
