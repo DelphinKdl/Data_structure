69fdee5fa211317dbea20161e0a4081ff77f9d2b
abdulazizasiri <aasiri@lion.lmu.edu>
2018-07-23 20:45:36 -0700

    remove some files

---
+++ linkedLists/Java/LinkedList.java
@@ -0,0 +1,58 @@
+public class LinkedList {
+    private Node head ;
+    private static int size ; 
+    public LinkedList(){
+        this.head = null; 
+    } 
+    public int getSize(){
+        return this.size;
+    }
+    public void add(int x){
+        Node newNode = new Node(x);
+        this.size++;
+        if (this.head == null){
+            this.head = newNode ;
+            return ; 
+        }
+        Node temp = this.head ; 
+        while (temp.next != null){
+            temp = temp.next ;
+        }
+        temp.next = newNode;
+        
+    }
+    public void print(){
+        Node temp = this.head ; 
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next; 
+        }
+    }
+    public static void main (String[] args){
+        LinkedList list = new LinkedList();
+     
+        list.add(12);
+        // System.out.println(list.getSize());
+        list.add(120);
+        list.add(12120);
+        // System.out.println(list.getSize());
+        list.print();
+        System.out.println(list.getSize());
+    }
+    boolean isEmpty (){
+        if (this.head == null){
+            return true;
+        }
+        else return false ;
+    }
+
+    // Provate class: Not visible to anyone. 
+    private class Node{
+        Node next; 
+        int data ; 
+        Node(int data) {
+            this.data = data ; 
+            this.next = null ;
+        }
+    }
+}
