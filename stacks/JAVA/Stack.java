65535de41d477ae712cf38e7c8c8aa3f1ec8b618
abdulazizasiri <aasiri@lion.lmu.edu>
2018-07-25 00:40:29 -0700

    adde some ADT

--- stacks/JAVA/Stack.java
+++ stacks/JAVA/Stack.java
@@ -2,3 +2,14 @@
 // Remove in the begin.
 // Everything is done from the head node.
 // Implementation will be done sometime later.
+
+
+// ADT
+
+// 1- push(Element)
+
+// pop() ; // Remove lement on the top.
+
+// peak();// Check the peak element by returing it only without REMOVING.
+
+// isEmpty(); check if the stack is emepty.
