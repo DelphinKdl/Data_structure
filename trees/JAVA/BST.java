f6869307518107a0dc3a59bef47490c285990917
abdulazizasiri <aasiri@lion.lmu.edu>
2018-07-28 17:38:20 -0700

    adding node recursivly

--- trees/JAVA/BST.java
+++ trees/JAVA/BST.java
@@ -8,6 +8,19 @@ public class BST {
     this.size = 0;
     this.root = null;
   }
+  public Node addrec(Node root, int data){
+    if(root == null){
+      root = new Node(data);
+    
+    }
+    else if (data <= root.text){
+      root.left = addrec(root.left, data);
+    }
+    else
+    root.right = addrec(root.right, data);
+
+    return root;
+  }
   public Node rootGetter(){
     return this.root;
   }
@@ -20,15 +33,22 @@ public class BST {
 
     public static void main (String [] args){
       BST tree = new BST();
-         tree.add(10);
-          tree.add(2);
-          tree.add(12);
-          tree.add(4);
-          tree.add(3);
-          tree.add(11);
-          tree.add(19);
-          Node x = tree.rootGetter();
-          tree.postOrder(x);
+      Node x = tree.rootGetter(); 
+        //  tree.add(10);
+        //   tree.add(2);
+        //   tree.add(12);
+        //   tree.add(4);
+        //   tree.add(3);
+        //   tree.add(11);
+        //   tree.add(19);
+         x=  tree.addrec(x,10);
+          x= tree.addrec(x,2);
+          x = tree.addrec(x,12);
+          x= tree.addrec(x,4);
+          x=tree.addrec(x,3);
+          x=tree.addrec(x,11);
+         x= tree.addrec(x,19);
+          x=tree.postOrder(x);
 
 
 
