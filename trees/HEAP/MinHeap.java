0061af8125ed911b7cf7bc8ff4a85a07e8fad220
abdulazizasiri <aasiri@lion.lmu.edu>
2018-08-02 17:36:02 -0700

    added some operations ADT fr a heap

--- trees/HEAP/MinHeap.java
+++ trees/HEAP/MinHeap.java
@@ -1,5 +1,26 @@
 public class MinHeap {
-    public static void main(String [] args){
+    private static int size ; 
+    private int minHeap [] ;
+    // Constructor is used 
+    MinHeap(){
+        size = 0 ;
+
+    }
+
+    public void add(){
+
+    }
+    public void remove(){
+
+    }
+
+    public void removeMin(){
+
+    }
+    public void getMin(){
         
+    }
+    public static void main(String [] args){
+
     }
 }
