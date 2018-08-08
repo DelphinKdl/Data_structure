dd70d7c834bff868998af01e933645b45251f797
abdulazizasiri <aasiri@lion.lmu.edu>
2018-08-07 21:53:15 -0700

    added a delete method

--- trees/HEAP/MaxHeap.java
+++ trees/HEAP/MaxHeap.java
@@ -9,29 +9,27 @@ public class MaxHeap {
         this.maxHeap = new int[capacity];
     }
     private void doubleArray(){
-        int [] temp = new int [capacity];
+        int [] temp = new int [capacity+1];
         
-        for (int i = 0 ; i<capacity ; i++){
+        for (int i = 1 ; i<capacity-1 ; i++){
             temp[i] = maxHeap[i];
         }
         maxHeap = new int [capacity*2];
 
-        for (int i = 0 ; i<capacity ; i++){
+        for (int i = 1 ; i<capacity-1 ; i++){
             maxHeap[i] = temp[i];
         }
         
     }
-    private void swap(int x, int y){
-        
-    }
-    private void bubbleUp(int index, int val){
+
+    private void bubbleUp(int index){
          while (index != 1){
                 if (this.maxHeap[index] > this.maxHeap[(index)/2]){
-                    int temp = this.maxHeap[index];
-                    this.maxHeap[index] = this.maxHeap[(index)/2];
-                    this.maxHeap[(index)/2] = temp;
-                    this.size++; 
-                
+                    int temp = this.maxHeap[index/2];
+                    this.maxHeap[index/2] = this.maxHeap[(index)];
+                    this.maxHeap[index] = temp;
+             } else {
+                 return;
              }
              index/=2;
          }
@@ -41,37 +39,73 @@ public class MaxHeap {
         // Check paranets, if greater than the parent, swap.
 
         if (this.size == 1){
-            maxHeap[1] = val ;
+            this.maxHeap[1] = val ;
             this.size++;
-            return; // 
+            return; 
         }
         if (size == this.capacity-1){
             doubleArray();
         }
         int index = this.size;
+        this.size++; 
         this.maxHeap[index] = val ;
         // After inserting, we need to check if it is in the proper position.
-        bubbleUp(index, val);
+        bubbleUp(index);
     }
-    public void remove(){
+    public void remove(int index){
+        if (index == 0){
+            System.out.println("Not allowed");
+            return; 
+        }
+        if (index  == this.size-1){
+            this.size--;
+            return;
+        }
+        int temp = this.maxHeap[this.size-1];
+        this.maxHeap[this.size-1] = this.maxHeap[index];
+        this.maxHeap[index] = temp ; 
+        this.size--;
+
+        // I am at the last level.
+        while ((index * 2) < (this.size-1) ){
+            if (this.maxHeap[index*2] > this.maxHeap[index*2+1] ){
+                int temp2 = this.maxHeap[index] ; 
+                this.maxHeap[index] = this.maxHeap[index*2];
+                this.maxHeap[index*2] = temp2 ;
+                index=index*2;
+            } else {
+                int temp2 = this.maxHeap[index] ; 
+                this.maxHeap[index] = this.maxHeap[index*2+1];
+                this.maxHeap[index*2+1] = temp2 ;
+         
+                index=index*2+1;
+            }
+        }
 
     }
     public int getMin(){
         return this.maxHeap[1];
     }
-    public void print(){
-        for (int i= 1 ; i<this.size-1 ; i++)
-        System.out.println(maxHeap[i]);
-    }
     public static void main(String [] args){
         MaxHeap heap = new MaxHeap(6);
-        heap.add(73);
-        heap.add(74);
-        heap.add(81);
-        heap.add(79);
-        heap.add(90);
-        heap.add(93);
+        heap.add(1);
+        heap.add(5);
+        heap.add(7);
+        heap.add(3);
+        heap.add(4);
+        heap.add(9);
+        heap.add(2);
+        heap.add(1);
+        heap.add(10);
+        // System.out.println("SIZE "+(heap.size-1));
+        // heap.remove(2);
+        // 8, 5, 6, 3,4, 1, 2,1
+            for (int i = 1 ; i<heap.size; i++){
+            System.out.println(": "+heap.maxHeap[i]);
+        }
+
+        // System.out.println("Last Eleement: "+(heap.size- 1));
         // heap.add(94);
-       heap.print();
+    //    heap.print();
     }
 }
