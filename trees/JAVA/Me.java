00bd73d1034997c2d540cc9d8a90b90b39c7b72d
abdulazizasiri <aasiri@lion.lmu.edu>
2018-09-29 17:02:27 -0700

    added some code

--- trees/JAVA/Me.java
+++ trees/JAVA/Me.java
@@ -5,7 +5,14 @@ public class Me {
     this.name = s ;
     System.out.println(this.name);
   }
+  public void method2(){
+    System.out.println(" Me MEthod 2");
+  }
+  public void method1(){
+    System.out.println(" Me MEthod 1");
+  }
   public static void main (String [] args){
-
+  Me s = new Sample("Aziz");
+  s.method1();
   }
 }
--- trees/JAVA/Sample.java
+++ trees/JAVA/Sample.java
@@ -4,8 +4,13 @@ public class Sample extends Me{
   Sample(String n){
     super(n);
   }
-  public static void main (String [] args){
-    // List<List<Integer>> list = new ArrayList<List<Integer>>();
-    Sample x = new Sample("Aziz");
+  public void method1(){
+    System.out.println(" SAMPLE MEthod 1");
+    method2(); // this refers to the calling object whihc is of type Sample.
+    // I know that nameing classes is awful. 
+  }
+
+  public void method2(){
+    System.out.println(" SAMPLE MEthod 2");
   }
 }
